d0a66acf1a30a03c211674109ff11b8388698f43
hytsnbr <haya524mwam@gmail.com>
2017-06-03 01:28:16 +0900

    コメント部を削除

--- app/src/main/java/hytsnbr10519/colorpicker/MainActivity.java
+++ app/src/main/java/hytsnbr10519/colorpicker/MainActivity.java
@@ -15,31 +15,12 @@ public class MainActivity extends AppCompatActivity implements OnSeekBarChangeLi
     private int _red, _green, _blue, _alpha;
     private TextView result;
 
-    /*
-    private SeekBar[] seekBars = new SeekBar[4];
-    private int[] colors = new int[4];
-    */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         color_view = findViewById(R.id.color_view);
-
-        /*
-        seekBars[0] = (SeekBar)findViewById(R.id.red);
-        seekBars[1] = (SeekBar)findViewById(R.id.green);
-        seekBars[2] = (SeekBar)findViewById(R.id.blue);
-        seekBars[3] = (SeekBar)findViewById(R.id.alpha);
-
-        for (int i = 0;i < seekBars.length;i++) {
-            seekBars[i].setMax(255);
-            seekBars[i].setOnSeekBarChangeListener(this);
-        }
-        seekBars[3].setProgress(255);
-        */
-
         red = (SeekBar)findViewById(R.id.red);
         green = (SeekBar)findViewById(R.id.green);
         blue = (SeekBar)findViewById(R.id.blue);
@@ -89,35 +70,16 @@ public class MainActivity extends AppCompatActivity implements OnSeekBarChangeLi
                 break;
         }
 
-        /*
-        switch (seekBar.getId()) {
-            case R.id.red:
-                colors[1] = progress;
-                break;
-            case R.id.green:
-                colors[2] = progress;
-                break;
-            case R.id.blue:
-                colors[3] = progress;
-                break;
-            case R.id.alpha:
-                colors[0] = progress;
-                break;
-        }
-        */
-
         setView();
         setText();
     }
 
     private void setView() {
-        //int color = Color.argb(colors[0], colors[1], colors[2], colors[3]);
         int color = Color.argb(_alpha, _red, _green, _blue);
         color_view.setBackgroundColor(color);
     }
 
     private String getTextString() {
-        //return getString(R.string.result, colors[0], colors[1], colors[2], colors[3]);
         return getString(R.string.result, _red, _green, _blue, _alpha);
     }
 
@@ -131,11 +93,7 @@ public class MainActivity extends AppCompatActivity implements OnSeekBarChangeLi
         _green = green.getProgress();
         _blue = blue.getProgress();
         _alpha = alpha.getProgress();
-        /*
-        for (int i = 0;i < seekBars.length;i++) {
-            colors[i] = seekBars[i].getProgress();
-        }
-        */
+        
         result.setText(getTextString());
     }
 }
